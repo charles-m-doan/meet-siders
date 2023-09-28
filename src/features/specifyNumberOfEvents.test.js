c3f39a72e3a4090b3f038054a52c17c814cf7127
charles-m-doan <cmatthewdoan@gmail.com>
2023-09-28 14:23:36 -0400

    Fixing some tests.

--- src/features/specifyNumberOfEvents.test.js
+++ src/features/specifyNumberOfEvents.test.js
@@ -7,19 +7,14 @@ import { getEvents } from '../mock-data';
 const feature = loadFeature('./src/features/specifyNumberOfEvents.feature');
 
 //Feature #3 :
-defineFeature(feature, test => {
-
+defineFeature(feature, (test) => {
    //Scenario #1:
-   test('When user hasn’t specified a number, 1 is the default number.', ({ given, when,then }) => {
-
-      let AppComponent;
-
+   test('When user hasn’t specified a number, 1 is the default number.', ({ given, when, then }) => {
       given('A list of events was loaded', () => {
-         AppComponent = render(<App />);
+         render(<App />);
       });
 
-      when('The list is dispalyed to the user with default settings', () => {
-      });
+      when('The list is displayed to the user with default settings', () => {});
 
       then('The list will display 1 event unless directed to show more', () => {
          const defaultNumber = screen.getByTestId('number-of-events-component').value;
@@ -28,20 +23,16 @@ defineFeature(feature, test => {
    });
 
    //Scenario #2:
-   test('User can change the number of events they want to see.', ({ given, when, then }) => {
-
-      let AppComponent;
-
-      given('A list of events was loaded', () => {
-         AppComponent = render(<App />);
+   test('User can change the number of events they want to see.', async ({ given, when, then }) => {
+      given('A list of events was loaded', async () => {
+         render(<App />);
+         await waitFor(() => expect(screen.getByTestId('event-list')).toBeInTheDocument()); // Wait for events to be loaded
       });
 
-      when('The user specifies how many events they want displayed at one time', async() => {
+      when('The user specifies how many events they want displayed at one time', async () => {
          const input = screen.getByTestId('number-of-events-component');
-         fireEvent.change(input, { target: { value: '1' } } );
-         await waitFor(() => {
-            expect(input.value).toBe('1');
-         });
+         fireEvent.change(input, { target: { value: '1' } });
+         await waitFor(() => expect(input.value).toBe('1')); // Wait for input value to change
       });
 
       then('A number of events will be displayed equal to the amount the user specified that they want displayed', () => {
@@ -50,5 +41,4 @@ defineFeature(feature, test => {
          expect(eventItems.length).toBe(1);
       });
    });
-
 });
